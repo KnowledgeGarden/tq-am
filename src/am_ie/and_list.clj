ce864945c2d703ebf5dfb3b3842d316636c1febf
Jack Park <jackpark@gmail.com>
2020-08-19 19:28:41 -0700

    Completed Issue 1 - deleted old inference engine code

--- src/am_ie/and_list.clj
+++
@@ -1,4 +0,0 @@
-(ns am-ie.and-list
-    (require am-ie.inferrable :as I) )
-
-; An and-list must extend the api of a List<Inferrable>
--- src/am_ie/inferrable.clj
+++
@@ -1,9 +0,0 @@
-(ns am-ie.inferrable)
-
-
-(defprotocol Inferrable
-    "API for an Inference Engine"
-    (setId [id] "set Id")
-    (getId [] "returns Id")
-    (setInferrable [inferrable] "set the Inferrable object")
-    (eval [] "Returns a boolean"))
